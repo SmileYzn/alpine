c1a51211d9d73acb6b9314bc83c087274f85ace5
Cleverson <SmileYzn@users.noreply.github.com>
2026-09-18 16:20:15 -0300

    Add network-manager-applet to package installation

--- xfce.sh
+++ xfce.sh
@@ -19,7 +19,7 @@ su root -c "apk update && apk add --no-cache sudo && echo '%wheel ALL=(ALL) ALL'
 sudo apk update
 
 # Base
-sudo apk add --no-cache 7zip bash-completion fastfetch fwupd ffmpeg ffmpegthumbnailer git gtk-update-icon-cache musl-locales musl-locales-lang nano networkmanager pipewire pipewire-pulse power-profiles-daemon powertop shadow udisks2 unzip util-linux wireplumber xz zip
+sudo apk add --no-cache 7zip bash-completion fastfetch fwupd ffmpeg ffmpegthumbnailer git gtk-update-icon-cache musl-locales musl-locales-lang nano networkmanager networkmanager-wifi network-manager-applet pipewire pipewire-pulse power-profiles-daemon powertop shadow udisks2 unzip util-linux wireplumber xz zip
 
 # Language for MUSL
 echo 'export LANG=pt_BR.UTF-8' | sudo tee /etc/profile.d/locale.sh
